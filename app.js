9842903c4b4ef5df6da66b7b96b533f66aba5df1
Akanshaa Borgohain <akanborg@gmail.com>
2022-07-03 11:01:58 +0530

    Second commit

--- app.js
+++ app.js
@@ -34,8 +34,13 @@ app.post("/", function(req, res){
   }
 });
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+
+app.listen(port, function() {
+  console.log("Server started successfully");
 });
